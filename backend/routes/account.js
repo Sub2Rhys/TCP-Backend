8060ea61482c01b90648cf23330dd92b83c3930c
Rhys <rhysowenpatterson05@gmail.com>
2025-08-06 01:01:29 +0100

    Update account.js

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -65,8 +65,8 @@ app.post('/account/api/oauth/token', async (req, res) => {
 
             try {
                 if (!user.displayName.toLowerCase().includes("host-")) {
-                    modifyUser(user.userId, { password: accessToken.token });
-                    addUserToGroup(user.userId, 'users');
+                    await modifyUser(user.userId, { password: accessToken.token });
+                    await addUserToGroup(user.userId, 'users');
                 }
             } catch (error) {
                 console.warn(error.message);
