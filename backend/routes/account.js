4f77249e6e8464ef97364a370b36d58b771ad83e
Rhys <59174048+Sub2Rhys@users.noreply.github.com>
2025-09-13 23:54:42 +0100

    Update account.js

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -285,4 +285,31 @@ app.delete('/account/api/oauth/sessions/kill/:token', requireAuth, async (req, r
     }
 });
 
+app.get('/api/launcher/login', async (req, res) => {
+    const { email, password } = req.query;
+
+    if (!email) return res.status(400).send('The email was not entered.');
+    if (!password) return res.status(400).send('The password was not entered.');
+
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(404).send('User not found.');
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+            const username = user.username;
+
+            return res.status(200).json({
+                username: username,
+            });
+        } else {
+            return res.status(400).send('Error!');
+        }
+    } catch (err) {
+        log.error('Launcher Api Error:', err);
+        return res.status(500).send('Error encountered, look at the console');
+    }
+});
+
 module.exports = app;
